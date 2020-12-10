0b5959e6c7abcb84fbefca3a27f38acd3bb53786
bpilseo <bpilseo@cau.ac.kr>
2020-12-10 19:04:52 +0900

    config.py

---
+++ config.py
@@ -0,0 +1,17 @@
+BATCH_SIZE = 128
+SAVE_FREQ = 1
+TEST_FREQ = 1
+TOTAL_EPOCH = 70
+
+RESUME = ''
+SAVE_DIR = './model'
+MODEL_PRE = 'CASIA_B512_'
+
+
+CASIA_DATA_DIR = 'C:\\Users\\pc\\Desktop\\datasets\\CASIA-WebFace\\CASIA-WebFace-160'
+LFW_DATA_DIR = 'C:\\Users\\pc\\Desktop\\datasets\\lfw-align-128'
+
+# CASIA_DATA_DIR = '/HDD/bpilseo/datasets/CASIA-WebFace/CASIA-WebFace-160'
+# LFW_DATA_DIR = '/HDD/bpilseo/datasets/lfw-align-128'
+
+GPU = 0
